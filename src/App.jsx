a4905aaecb7f9e4ab769bb28175cd57379ffb393
Vriddhi Vashi <vvriddhi@gmail.com>
2025-09-19 08:45:57 +0530

    basic

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+  
+  return (
+      <div className='text-3xl font-bold underline text-blue-500'>
+        hello tailwind is working guys!!
+      </div>
+
+  )
+}
+
+export default App
